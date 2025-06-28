d35fbe79aab93cd5856566b8d9442d5df8c68f21
Rob Swindells <robgramming@users.noreply.github.com>
2025-06-28 12:10:42 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
+// import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={`${styles.main} ${inter.className}`}>
 
        <h1>Congratulations!</h1>
-  <p>You've completed the essentials of automated application deployment with GitHub Actions! 🥳</p>
+  <p>You&apos;ve completed the essentials of automated application deployment with GitHub Actions! 🥳</p>
 
       </main>
     </>
